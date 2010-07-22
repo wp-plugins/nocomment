366e5ae79c0127793f96234dd7f900c21b07e9b7
pasxal <pasxal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-22 10:34:35 +0000

    Version 1.0a
    git-svn-id: https://plugins.svn.wordpress.org/nocomment/trunk@265832 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ noComment.php
@@ -0,0 +1,62 @@
+<?php  
+/*
+Plugin Name: noComment
+Plugin URI: http://adigit.gr/
+Description: The noComment Plugin. A plugin that shows you the post that noone wants to comment. :P
+Version: 1.0a
+Author: pasxal
+Author URI: http://adigit.gr/
+License: GPL2
+*/
+
+
+class noComment extends WP_Widget {  
+    function noComment() {  
+        parent::WP_Widget(false, $name = 'noComment');  
+    }  
+  
+    //Widget
+    function widget($args, $instance) {  
+        extract( $args );  
+	echo $before_widget;
+	echo $before_title . $instance['title'] . $after_title; 
+			
+	
+		      //Functionality
+		        global $wpdb;
+			$query="SELECT * FROM ".$wpdb->prefix."posts WHERE post_status='publish' AND comment_status='open' AND comment_count='0' ORDER BY post_date DESC LIMIT ".$instance['num'];
+			$results = $wpdb->get_results($query);
+			
+				if(!empty($results))
+				{
+					echo "<ul>";
+					foreach ($results as $res)
+					{
+						echo "<li><a href=\"$res->guid\">".$res->post_title."</a></li>";
+					}
+					echo "</ul>";
+				}else{echo "No posts without comments found.";}
+
+                  
+               echo $after_widget;   
+  
+    }  
+    
+    //Options
+    function form($instance) {  
+        $title = esc_attr($instance['title']);  
+        $num = esc_attr($instance['num']);  
+	?>
+            <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>  
+            <p><label for="<?php echo $this->get_field_id('num'); ?>"><?php _e('Number of posts:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('num'); ?>" name="<?php echo $this->get_field_name('num'); ?>" type="text" value="<?php echo $num; ?>" /></label></p>  
+            
+        <?php  
+    }  
+    //Update
+    function update($new_instance, $old_instance) {  
+        return $new_instance;  
+    }  
+}  
+//Reg it
+add_action('widgets_init', create_function('', 'return register_widget("noComment");'));  
+?>  
---
+++ readme.txt
@@ -0,0 +1,21 @@
+=== noComment ===
+Contributors: pasxal
+Tags: noComment, comments
+Requires at least: 2.8
+Tested up to: 3.0
+Stable tag: 1.0a
+
+== Description ==
+
+The noComment Plugin. A plugin that shows you the post that noone wants to comment. :P
+
+== Installation ==
+
+Upload the noComment plugin to your blog, and activate it.
+
+I think you're done!
+
+= 1.0a =
+
+* Functional :P
+
